151827b69cc8870e459e8eb70f65c0d4abd16d4d
Ali <sr.ali.torabi@gmail.com>
2023-09-04 22:11:27 +0330

    add exist method

--- dlcore_errors.go
+++ dlcore_errors.go
@@ -2,5 +2,5 @@ package dbcore
 
 import "github.com/aliworkshop/error"
 
-var NotFoundErr = error.NotFound(nil).WithCode(100)
-var NotEnoughParams = error.Validation(nil).WithCode(101)
+var NotFoundErr = error.NotFound(nil).WithCode(404)
+var NotEnoughParams = error.Validation(nil).WithCode(422)
--- repository.go
+++ repository.go
@@ -15,6 +15,7 @@ type Repository interface {
 	List(query QueryModel) (items interface{}, err error.ErrorModel)
 	ListWithDFilter(query QueryModel) (items interface{}, err error.ErrorModel)
 	Get(query QueryModel) (item interface{}, err error.ErrorModel)
+	Exist(query QueryModel) (exists bool, err error.ErrorModel)
 
 	Insert(query QueryModel) (result interface{}, err error.ErrorModel)
 	Upsert(query QueryModel) (err error.ErrorModel)
